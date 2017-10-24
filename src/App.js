60810a4270ed43201e093c3ad9991187c234479e
Erin <erinedawson@gmail.com>
2017-10-24 15:17:45 -0400

    install dependencies.  add Books Container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import axios from 'axios';
+import {BrowserRouter as Router,
+        Route,
+        Link,
+        Redirect
+} from 'react-router-dom';
 import './App.css';
+import BooksContainer from './BooksContainer'
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <Router>
+
+
+      <div className="content">
+
+        <header className="header">
+
+          <h1 className="title">Welcome to Bookworm</h1>
+          <h3>Improving literary access for District residents.</h3>
+
+          <nav clasName="nav">
+            <div className="nav-left">
+              <a>Where To Read</a>
+              <a>What To Read</a>
+            </div>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="container">
+          <img className="jumbotron"src="http://ichef.bbci.co.uk/wwfeatures/wm/live/1280_640/images/live/p0/54/q9/p054q93q.jpg"  alt="books on a shelf"/>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              1 of 3
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              2 of 3
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col">
+              3 of 3
+            </div>
+          </div>
+
+        </div>
+
+        <div className="sidebar">
+          <h3>Resources</h3>
+        </div>
+
       </div>
+    </Router>
     );
   }
 }
---
+++ src/BooksContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+
+
+class BooksContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      titleQuery: this.titleQuery
+    }
+  }
+  render(){
+    return(
+      <div className="container">
+        <h3>Search for books here.</h3>
+
+      
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default BooksContainer
